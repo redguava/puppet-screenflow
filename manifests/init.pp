17ae1fb510f6bc0496a9ac23554ba298abf92e97
Matthew Jones <matt@geekle.id.au>
2013-09-16 12:35:56 +1000

    Initial commit.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,11 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install ScreenFlow.app into /Applications.
+#
+# Examples
+#
+#   include screenflow
+class screenflow {
+  package { 'ScreenFlow':
+    provider => 'appdmg',
+    source   => 'http://www.telestream.net/download-files/screenflow/4-0/ScreenFlow-4.0.5.dmg'
+  }
 }
