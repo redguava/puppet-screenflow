9e86d3d676ecc2d3771fae69d0238c50cbf36011
Kerim Satirli <k.satirli@synapticate.com>
2014-03-02 11:06:10 +0100

    updated URL to version 4.5 of Screenflow
    I have updated the source URL to download the latest version (4.5) of Screenflow.

--- spec/classes/screenflow_spec.rb
+++ spec/classes/screenflow_spec.rb
@@ -4,7 +4,7 @@ describe 'screenflow' do
   it do
     should contain_package('ScreenFlow').with({
       :provider => 'appdmg',
-      :source   => 'http://www.telestream.net/download-files/screenflow/4-0/ScreenFlow-4.0.5.dmg',
+      :source   => 'http://www.telestream.net/download-files/screenflow/4-5/ScreenFlow-4.5.dmg',
     })
   end
 end
